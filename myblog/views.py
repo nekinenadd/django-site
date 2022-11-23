a6fdcf0ae656fb4e2e067c11f758e7dd79038992
Nenad Laptop <nenadsombor.rs@gmail.com>
2022-11-23 12:09:10 +0100

    views and urls

--- myblog/views.py
+++ myblog/views.py
@@ -3,12 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 
 def starting_page(request):
-    pass
+    return render(request,'myblog/index.html')
 
 
 def posts(request):
-    pass
+    return render(request,'myblog/posts.html')
 
 
-def post_detail(request):
-    pass
+def post_detail(request,slug):
+    return render(request, 'myblog/post-detail.html')
